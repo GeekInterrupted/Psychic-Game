773bab980eab16f5e6ab65e0728f2edbc9a82247
GeekInterrupted <GeekInterrupted@users.noreply.github.com>
2017-03-26 01:47:39 -0500

    revised version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,57 @@
+//psychic game v2.0
+//initialize variables
 var wins = 0;
 var losses = 0;
-var guessCount = 0;
-var guessesLeft = 5;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
+
+
+//select random letter
 var randomLetter = String.fromCharCode(97 + Math.floor(Math.random() * 26));
-var gameOver = false;
-        
-        
-        playGame();
-        
-        function playGame() {       
-       
-                    
-        var userText = document.getElementById("user-text");
-        document.onkeyup = function(event) {
-		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-            
-            console.log("computer", randomLetter);
-            
-                
-          if (userGuess === randomLetter) {   
-                wins++; 
-                document.getElementById("message-wins").innerHTML= "Wins: "+ wins;
-                var cumulativeUserGuesses = userGuess;
-		        document.getElementById("mydiv").insertAdjacentHTML('beforeend', cumulativeUserGuesses + ", ");
-                		              
-            } if (userGuess !== randomLetter) {
-                losses++;
-                guessesLeft--;
-                document.getElementById("message-losses").innerHTML= "Losses: "+ losses;
-                var cumulativeUserGuesses = userGuess;
-		        document.getElementById("mydiv").insertAdjacentHTML('beforeend', cumulativeUserGuesses + ", ");
-                document.getElementById("guesses-left").innerHTML= "You have "+ guessesLeft + " guesses left";
-                                             
-            }   if (guessesLeft === 0) {
-               
-                gameOver = true;
-                document.getElementById("computer-choice").innerHTML= "HAL chose: "+ randomLetter;
-                randomLetter=String.fromCharCode(97 + Math.floor(Math.random() * 26)); 
-                console.log(gameOver);
-                } if (gameOver===true){ 
-//                    document.getElementById("computer-choice").style.visibility = "hidden";
-                    guessesLeft=5;
-                    losses=0;
-                    gameOver = false;
-                    
-                }
-            
-            
-        }
-    }
-        
-        
+//get user input
+var userText = document.getElementById("user-text");
+    document.onkeyup = function(event) {
+//convert to lowercase and make array to store user choices    
+var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+var cumulativeUserGuesses =guessedLetters.push(userGuess);
+        document.getElementById("mydiv").innerHTML = guessedLetters;
+        console.log(guessesLeft);
+        console.log("computer", randomLetter);
+
+//clear values        
+var reset = function() {
+    totalGuesses = 9;
+    guessesLeft = 10;
+    guessedLetters = [];
+    randomLetter = String.fromCharCode(97 + Math.floor(Math.random() * 26));
+    letterToGuess = null;
+    
+    
+    
+}
+   
+    if (userGuess === randomLetter) {   
+        wins++; 
+        document.getElementById("message-wins").innerHTML = "Wins: " + wins;    
+        } if (userGuess !== randomLetter) {
+             guessesLeft--;
+             document.getElementById("guesses-left").innerHTML = "Guesses left: " + guessesLeft;
+            } if (guessesLeft === 0) {
+                    losses++;
+                    document.getElementById("message-losses").innerHTML = "Losses: " + losses;
+//                    document.getElementById("computer-choice").innerHTML = "HAL chose: " + randomLetter;
+                    reset();
+ }   
+}
+    
+ 
+
+
+    
+
+
+   
+
+ 
